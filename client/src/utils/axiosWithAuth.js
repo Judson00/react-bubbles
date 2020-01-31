6ee6aad351ada75ff9ba222de8529605794cac71
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-31 13:51:37 -0500

    login page built

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
 export const axiosWithAuth = () => {
-  let token = localStorage.getItem(`token`)
+  
   return Axios.create({
     baseURL: "http://localhost:5000",
     headers: {
-      Authorization: token;
+      Authorization: localStorage.getItem(`token`)
     }
   });
 };
